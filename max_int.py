f2170f9b7b0c648dc73c0976981a44256f530e76
Kolbrún Rut Olsen <KolbrunOlsen@Kolbruns-MacBook-Air-4.local>
2019-09-05 15:26:07 +0000

    Sending files to GitHub

--- max_int.py
+++ max_int.py
@@ -8,5 +8,5 @@ while 0 <= num_int:
         max_int = num_int
     num_int = int(input("Input a number: ")) 
 
-print("The maximum is", max_int)    # Do not change this line
+print("The maximum is", max_int)
 
